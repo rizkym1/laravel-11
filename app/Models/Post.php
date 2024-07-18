5421c64f06882f7074a6ceeb3ba40fad0e70bba0
rizkym1 <rizkimaulana3216@gmail.com>
2024-07-18 21:41:50 +0700

    factory

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,44 +2,11 @@
 
 namespace App\Models;
 
-use Illuminate\Support\Arr;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
-class Post
+class Post extends Model
 {
-    public static function all()
-    {
-        return
-            [
-                [
-                    'id' => 1,
-                    'slug' => 'judul-artikel-1',
-                    'title'  => 'Judul Artikel 1',
-                    'author' => 'Rizky Maulana',
-                    'body'   => 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eveniet omnis, vero ab
-                               facilis nisi qui. Facilis, possimus illum necessitatibus, est praesentium placeat deserunt enim rerum quis magni hic ipsum quas.'
-                ],
-                [
-                    'id' => '2',
-                    'slug' => 'judul-artikel-2',
-                    'title'  => 'Judul Artikel 2',
-                    'author' => 'Rizky Maulana',
-                    'body'   => 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eveniet omnis, vero ab
-                               facilis nisi qui. Facilis, possimus illum necessitatibus, est praesentium placeat deserunt enim rerum quis magni hic ipsum quas.'
-                ]
-            ];
-    }
-
-    public static function find($slug)
-    {
-        // return Arr::first(static::all(), function ($post) use ($slug) {
-        //     return $post['slug'] == $slug;
-        // });
-
-        $post = Arr::first(static::all(), fn ($post) => $post['slug'] == $slug);
-
-        if (!$post) {
-            abort(404);
-        }
-        return $post;
-    }
+    use HasFactory;
+    protected $fillable = ['title', 'author', 'slug', 'body'];
 }
--- routes/web.php
+++ routes/web.php
@@ -17,10 +17,10 @@ Route::get('/posts', function () {
     return view('posts', ['title' => 'Blog', 'posts' => Post::all()]);
 });
 
-route::get('/posts/{slug}', function ($slug) {
+route::get('/posts/{post:slug}', function (Post $post) {
 
     //mencari elemen array yang ketemu berdasarkan kriteria tertentu
-    $post = Post::find($slug);
+    //$post = Post::find($id);
     return view('post', ['title' => 'Single Post', 'post' => $post]);
 });
 
